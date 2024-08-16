497e44c89877cd3349965456ddc10fff91e0513e
hand-stencil <gnsia@naver.com>
2024-08-16 12:44:08 +0000

    More Await in PostDetail Component

--- src/component/posts/PostDetail.js
+++ src/component/posts/PostDetail.js
@@ -1,19 +1,37 @@
 import Component from "../../core/Component.js";
 
 export default class PostDetail extends Component {
-    template() {
+    setup() {
         const post = this.getPost();
+        this.state = { post };
+    }
+    mounted() {
+        const { $target } = this;
+        const { date, title, img, description } = this.state.post;
+        
+        const $date = $target.querySelector('[data-post="date"]');
+        const $title = $target.querySelector('[data-post="title"]');
+        const $img = $target.querySelector('[data-post="img"]');
+        const $description = $target.querySelector('[data-post="description"]');
+
+        $date.innerHTML = date
+        $title.innerHTML = title;
+        $img.innerHTML = img;
+        $description.innerHTML = description;
+    }
+    template() {
         return `
             <h3>PostDetail</h3>
-            <h3>${post.default.date}</h3>
-            <h3>${post.default.title}</h3>
-            <h3>${post.default.img}</h3>
-            <h3>${post.default.description}</h3>
+            <h3 data-post="date">${post.default.date}</h3>
+            <h3 data-post="title">${post.default.title}</h3>
+            <h3 data-post="img">${post.default.img}</h3>
+            <h3 data-post="description">${post.default.description}</h3>
         `;
     }
     async getPost() {
         const { date } = this.props;
         const path = `../../../assets/posts/${date}/POST.js`;
-        return await import(path);
+        const post = await import(path);
+        return post.default;
     }
 }
