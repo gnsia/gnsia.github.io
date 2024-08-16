fa1546adcd2288f073637b4038c0444c55ad2b12
hand-stencil <gnsia@naver.com>
2024-08-16 13:27:22 +0000

    Add Show Loading Interval

--- src/component/posts/PostDetail.js
+++ src/component/posts/PostDetail.js
@@ -3,7 +3,8 @@ import Component from "../../core/Component.js";
 export default class PostDetail extends Component {
     setup() {
         this.state = { 
-            post: null
+            post: null,
+            loadingInterval: null,
         };
         this.getPost();
     }
@@ -18,13 +19,26 @@ export default class PostDetail extends Component {
             <h3 data-post="description">${description}</h3>
         `;
         } else {
-            return `<h3>please wait!</h3>`
+            return `<h3>${this.showLoading()}</h3>`
         }
+    }
+    showLoading(idx) {
+        this.state.loadingInterval = setInterval(() => {
+            const text = 'please wait!';
+            const random = this.getRandom(0, 10);
+            const char = text[random];
+            text.replace(char, char.toLocaleUpperCase());
+            return text;
+        }, 100);
+    }
+    getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
     async getPost() {
         const { date } = this.props;
         const path = `../../../assets/posts/${date}/POST.js`;
         const post = await import(path);
+        clearInterval(this.state.loadingInterval);
         this.changePostHandler(post.default);
     }
     changePostHandler(post) {
