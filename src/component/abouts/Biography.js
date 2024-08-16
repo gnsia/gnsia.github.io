ab0813cbe086e8839464b15c081686683850e734
hand-stencil <gnsia@naver.com>
2024-08-16 04:55:04 +0000

    create introduction, biography, discography

---
+++ src/component/abouts/Biography.js
@@ -0,0 +1,9 @@
+import Component from "../../core/Component.js";
+
+export default class Biography extends Component {
+    template() {
+        return `
+            <h3>Biography</h3>
+        `;
+    }
+}
---
+++ src/component/abouts/Discography.js
@@ -0,0 +1,9 @@
+import Component from "../../core/Component.js";
+
+export default class Discography extends Component {
+    template() {
+        return `
+            <h3>Discography</h3>
+        `;
+    }
+}
