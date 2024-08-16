3fa04ccedc5f2363acf7fae55df9d9ced33c2c14
hand-stencil <gnsia@naver.com>
2024-08-16 07:59:57 +0000

    create PostList and PostDetail

--- src/component/pages/Posts.js
+++ src/component/pages/Posts.js
@@ -1,12 +1,42 @@
 import Component from "../../core/Component.js";
+import PostDetail from "../posts/PostDetail.js";
+import PostList from "../posts/PostList.js";
 
 export default class Posts extends Component {
+    setup() {
+        this.state = {
+            mode: 'list', // list, detail
+        }
+    }
+    mounted() {
+        const { $target, changeModeHandler } = this;
+        const { mode } = this.state;
+        const $child = $target.querySelector(`[data-component="${mode}"]`);
+
+        switch(mode) {
+            case 'list':
+                new PostList($child, { 
+                    changeModeHandler: changeModeHandler.bind(this),
+                });
+                break;
+            case 'detail':
+                new PostDetail($child, {
+                    changeModeHandler: changeModeHandler.bind(this),
+                });
+                break;
+            default:
+                alert(`${mode} is not available keyword reload please!`);
+                break;
+        }
+    }
     template() {
+        const { mode } = this.state;
         return `
             <h2>Posts</h2>
-            <ol>
-                <li><a href="javascript:void(0)">[20240816] 오늘의 일기~ 뻥입니다.</li>
-            </ol>
+            <div data-component="${mode}"></div>
         `;
+    }
+    changeModeHandler(mode) {
+        this.setState({ mode });
     }
 }
