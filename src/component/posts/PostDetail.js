dcc878c88e3a61df6649b9dea64c59056a48aa84
hand-stencil <gnsia@naver.com>
2024-08-16 12:55:08 +0000

    Add please wait in PostDetail Component

--- src/component/posts/PostDetail.js
+++ src/component/posts/PostDetail.js
@@ -2,36 +2,33 @@ import Component from "../../core/Component.js";
 
 export default class PostDetail extends Component {
     setup() {
-        const post = this.getPost();
-        this.state = { post };
-    }
-    mounted() {
-        const { $target } = this;
-        const { date, title, img, description } = this.state.post;
-
-        const $date = $target.querySelector('[data-post="date"]');
-        const $title = $target.querySelector('[data-post="title"]');
-        const $img = $target.querySelector('[data-post="img"]');
-        const $description = $target.querySelector('[data-post="description"]');
-
-        $date.innerHTML = date
-        $title.innerHTML = title;
-        $img.innerHTML = img;
-        $description.innerHTML = description;
+        this.state = { 
+            post: null
+        };
+        this.getPost();
     }
     template() {
-        return `
+        const { post } = this.state;
+        if(post) {
+            const { date, title, img, description } = post;
+            return `
             <h3>PostDetail</h3>
-            <h3 data-post="date"></h3>
-            <h3 data-post="title"></h3>
-            <h3 data-post="img"></h3>
-            <h3 data-post="description"></h3>
+            <h3 data-post="date">${date}</h3>
+            <h3 data-post="title">${title}</h3>
+            <h3 data-post="img">${img}</h3>
+            <h3 data-post="description">${description}</h3>
         `;
+        } else {
+            return `<h3>please wait!</h3>`
+        }
     }
     async getPost() {
         const { date } = this.props;
         const path = `../../../assets/posts/${date}/POST.js`;
         const post = await import(path);
-        return post.default;
+        this.changePostHandler(post.default);
+    }
+    changePostHandler(post) {
+        this.setState({ post })
     }
 }
