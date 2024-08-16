efed22c6010adadcae5d922ea8c43232de0754c5
hand-stencil <gnsia@naver.com>
2024-08-16 06:20:46 +0000

    Edit home component

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -4,6 +4,11 @@ export default class Home extends Component {
     template() {
         return `
             <h1>Welcome to my homepage!</h1>
+            <h2>Welcome to my homepage!</h1>
+            <h3>Welcome to my homepage!</h1>
+            <h4>Welcome to my homepage!</h1>
+            <h5>Welcome to my homepage!</h1>
+            <h6>Welcome to my homepage!</h1>
         `;
     }
 }
