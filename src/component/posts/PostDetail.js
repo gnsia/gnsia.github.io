6428b9cc03e77574901cc7760972b24ac1653674
hand-stencil <gnsia@naver.com>
2024-08-16 12:29:28 +0000

    Direct Return post in PostDetail

--- src/component/posts/PostDetail.js
+++ src/component/posts/PostDetail.js
@@ -5,16 +5,15 @@ export default class PostDetail extends Component {
         const post = this.getPost();
         return `
             <h3>PostDetail</h3>
-            <h3>${post.date}</h3>
-            <h3>${post.title}</h3>
-            <h3>${post.img}</h3>
-            <h3>${post.description}</h3>
+            <h3>${post.default.date}</h3>
+            <h3>${post.default.title}</h3>
+            <h3>${post.default.img}</h3>
+            <h3>${post.default.description}</h3>
         `;
     }
     async getPost() {
         const { date } = this.props;
         const path = `../../../assets/posts/${date}/POST.js`;
-        const post = await import(path);
-        return post.default;
+        return await import(path);
     }
 }
