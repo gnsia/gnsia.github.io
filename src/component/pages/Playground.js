f79e3e1220e80597628f5722f62d5451dd55b094
hand-stencil <gnsia@naver.com>
2024-08-19 02:21:51 +0000

    change h2 from h1 in Playground

--- src/component/pages/Playground.js
+++ src/component/pages/Playground.js
@@ -3,7 +3,7 @@ import Component from "../../core/Component.js";
 export default class Playground extends Component {
     template() {
         return `
-            <h1>Playground</h1>
+            <h2>Playground</h2>
             <ol>
                 <li><a href="javascript:void(0)">Interactive Developer's Kinectic Typography</a></li>
             </ol>
