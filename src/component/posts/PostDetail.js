6b7db660ecd46e5f28947d9b2810a0f34727d7d9
hand-stencil <gnsia@naver.com>
2024-08-19 02:13:30 +0000

    change Post Detail

--- src/component/posts/PostDetail.js
+++ src/component/posts/PostDetail.js
@@ -28,7 +28,6 @@ export default class PostDetail extends Component {
         const { changeModeHandler } = this.props;
         $target.addEventListener('click', ({ target }) => {
             if(target.tagName === 'A') {
-                alert('clicked');
                 changeModeHandler('list');
             }
         });
