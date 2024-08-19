678c14454e86e391c5444ba70e9eaf68e7e76975
hand-stencil <gnsia@naver.com>
2024-08-19 23:50:59 +0000

    2024/08/16 POST change

--- assets/posts/2024/08/16/POST.js
+++ assets/posts/2024/08/16/POST.js
@@ -1,8 +1,9 @@
 export default {
     date: '2024/08/16',
     title: '이거 어떻게 만드러?',
-    img: '',
+    img: 'https://picsum.photos/200/300',
     description: `
-        태어나서 이런거는 만들어 본 적이 없습니다. 살려주세요!!!
+        태어나서 이런거는 만들어 본 적이 없습니다. <br/>
+        살려주세요!!!
     `
 }
