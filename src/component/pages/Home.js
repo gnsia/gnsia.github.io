dd725c00ce42429aa7508054e07653da5b78cf00
hand-stencil <gnsia@naver.com>
2024-08-19 08:28:45 +0000

    add logo in home

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -3,6 +3,7 @@ import Component from "../../core/Component.js";
 export default class Home extends Component {
     template() {
         return `
+            <img src="./assets/img/logo/logo.png" alt="logo" width="300px"/>
             <h1>Welcome to my homepage!</h1>
             <h2>Welcome to my homepage!</h1>
             <h3>Welcome to my homepage!</h1>
