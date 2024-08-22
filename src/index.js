d37690244412c5e6d735fffb107a04a41d84d8a2
hand-stencil <gnsia@naver.com>
2024-08-22 00:46:22 +0000

    set index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-import App from "./component/App.js";
-new App(document.querySelector('#root'));
+class Index {
+    $root;
+    constructor($root) {
+        this.$root = $root;
+        this.render();
+        this.importApp();
+    }
+    render() {
+        const { $root } = this;
+        $root.innerHTML = `
+            <h1>L</h1>
+            <h1>O</h1>
+            <h1>A</h1>
+            <h1>D</h1>
+            <h1>I</h1>
+            <h1>N</h1>
+            <h1>G</h1>
+        `;
+    }
+    async importApp() {
+        const { $root } = this;
+        const app = await import("./component/App.js");
+        new app.default($root);
+    }
+}
+
+new Index(document.querySelector('#root'));
