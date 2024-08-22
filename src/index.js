3fcaf14e61582d830d90933f9d15c775484e99db
hand-stencil <gnsia@naver.com>
2024-08-22 04:19:59 +0000

    change index.js structure

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-class Index {
-    $root;
-    constructor($root) {
-        this.$root = $root;
-        this.render();
-        this.importApp();
-    }
-    render() {
-        const { $root } = this;
-        $root.innerHTML = `<h1>Hand-Stencil</h1>`;
-    }
-    async importApp() {
-        const { $root } = this;
-        const app = await import("./component/App.js");
-        new app.default($root);
-    }
-}
+import App from "./component/App.js";
 
-new Index(document.querySelector('#root'));
+new App(document.querySelector('#root'))
