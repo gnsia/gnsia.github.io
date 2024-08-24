564a96442fd53fd79a7137c67262b431c4bf3f77
donghun kim <gnsia@naver.com>
2024-08-24 23:46:07 +0900

    delete every css without cover animation...

--- src/component/abouts/Introduction.js
+++ src/component/abouts/Introduction.js
@@ -6,11 +6,6 @@ export default class Introduction extends Component {
             <h3>Introduction</h3>
             <img src="./assets/img/logo/logo.png" alt="logo" width="300px"/>
             <h4>Hello! I'm Kidohu</h4>
-            <h4>I'm a Musician</h4>
-            <h4>I'm a Singer Songwriter</h4>
-            <h4>I'm a Guitarist</h4>
-            <h4>I'm a Programmer</h4>
-            <h4>I'm a Web Developer</h4>
         `;
     }
 }
