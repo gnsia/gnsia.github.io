39953a6cf54a2b59dae4acffbbd4b2ec23dc9954
donghun kim <gnsia@naver.com>
2024-08-27 22:29:10 +0900

    change img loaded callback Introduction.js Component

--- src/component/abouts/Introduction.js
+++ src/component/abouts/Introduction.js
@@ -3,9 +3,17 @@ import Component from "../../core/Component.js";
 export default class Introduction extends Component {
     template() {
         return `
-            <h3>Introduction</h3>
+            <h3 class="loading">Introduction</h3>
             <img src="./assets/img/logo/logo.png" alt="logo" width="300px"/>
-            <h4>Hello! I'm Kidohu</h4>
+            <h4 class="hide">Hello! I'm Kidohu</h4>
         `;
+    }
+    setEvent() {
+        const { $el } = this;
+        const img = $el.querySelector('img');
+        img.addEventListener('load', ({ target }) => {
+            img.previousElementSibling.classList.toggle('loading');
+            img.nextElementSibling.classList.toggle('hide');
+        })
     }
 }
