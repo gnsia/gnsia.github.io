5bc80a708310e04befacd2510b312c25d666d462
donghun kim <gnsia@naver.com>
2024-10-07 09:21:44 +0900

    some changes...

--- src/component/Header/Title.js
+++ src/component/Header/Title.js
@@ -2,6 +2,6 @@ import Component from "../../core/Component.js";
 
 export default class Title extends Component {
     template() {
-        return `<h1 class="inline">Hand-Stencil</h1>`;
+        return `<h2 class="inline">Hand-Stencil</h2>`;
     }
 }
