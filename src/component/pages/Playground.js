094efa19c9581f2bb0a103409d8058b03eed88b6
hand-stencil <gnsia@naver.com>
2024-10-10 07:03:30 +0000

    change canvas size

--- src/component/pages/Playground.js
+++ src/component/pages/Playground.js
@@ -5,12 +5,21 @@ import Canvas from "../playground/Canvas.js";
 export default class Playground extends Component {
     template() {
         return `
-            <canvas id="canvas" width="300" height="300"></canvas>
+            <canvas id="canvas"></canvas>
         `;
     }
     mounted() {
+        const { $target } = this;
         var canvas = document.getElementById("canvas");
+        canvas.height = $target.clientHeight;
+        canvas.width = $target.clientWidth;
+
         var ctx = canvas.getContext("2d");
-        new Canvas(ctx);
+
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect(30, 30, 50, 50);
     }
 }
--- src/component/playground/Canvas.js
+++ src/component/playground/Canvas.js
@@ -6,10 +6,5 @@ export default class Canvas {
     }
     draw() {
         const { ctx } = this;
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(10, 10, 50, 50);
-
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(30, 30, 50, 50);
     }
 }
