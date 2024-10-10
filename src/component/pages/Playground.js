d4835ff25709f549f7cc37cec096f05f73505bff
hand-stencil <gnsia@naver.com>
2024-10-10 06:19:02 +0000

    change canvas

--- src/component/pages/Playground.js
+++ src/component/pages/Playground.js
@@ -16,12 +16,15 @@ export default class Playground extends Component {
             canvas.height = $target.innerHeight;
             
             var ctx = canvas.getContext("2d");
-
-            ctx.fillStyle = "rgb(200,0,0)";
-            ctx.fillRect(10, 10, 50, 50);
-
-            ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-            ctx.fillRect(30, 30, 50, 50);
+            ctx.beginPath();
+            ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+            ctx.moveTo(110, 75);
+            ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+            ctx.moveTo(65, 65);
+            ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+            ctx.moveTo(95, 65);
+            ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+            ctx.stroke();
         }
     }
 }
