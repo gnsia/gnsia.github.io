353b29932db4a8e10f5642e957cbc9ee024b7d29
donghun kim <gnsia@naver.com>
2024-10-11 11:32:03 +0900

    change playground structure

--- src/component/pages/Playground.js
+++ src/component/pages/Playground.js
@@ -3,32 +3,48 @@ import Canvas from "../playground/Canvas.js";
 
 
 export default class Playground extends Component {
-    template() {
-        return `
-            <canvas id="myCanvas" width="512" height="512"></canvas>
+  template() {
+    return `
+            <canvas id="myCanvas"></canvas>
         `;
+  }
+  setEvent() {
+    window.addEventListener('resize', () => {
+      this.resizeCanvas();
+    });
+  }
+  resizeCanvas() {
+    const canvas = document.getElementById('myCanvas');
+    const parent = document.querySelector('main');
+
+    canvas.width = parent.clientWidth > 720 ? 720 : parent.clientWidth;
+    canvas.height = parent.clientHeight;
+    this.draw();
+  }
+  draw() {
+    const canvas = document.getElementById('myCanvas');
+    const ctx = canvas.getContext('2d');
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 3; j++) {
+        ctx.beginPath();
+        var x = 25 + j * 50; // x coordinate
+        var y = 25 + i * 50; // y coordinate
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+        var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
+
+        ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+        if (i > 1) {
+          ctx.fill();
+        } else {
+          ctx.stroke();
+        }
+      }
     }
-    mounted() {
-        const canvas = document.getElementById('myCanvas');
-        const context = canvas.getContext('2d');
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < 3; j++) {
-              ctx.beginPath();
-              var x = 25 + j * 50; // x coordinate
-              var y = 25 + i * 50; // y coordinate
-              var radius = 20; // Arc radius
-              var startAngle = 0; // Starting point on circle
-              var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-              var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
-      
-              ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-      
-              if (i > 1) {
-                ctx.fill();
-              } else {
-                ctx.stroke();
-              }
-            }
-          }
-    }
+  }
+  mounted() {
+    this.resizeCanvas();
+  }
 }
