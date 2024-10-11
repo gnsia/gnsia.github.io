e647b3135b266f989bdc6edc6f108ce61f1b5153
donghun kim <gnsia@naver.com>
2024-10-11 11:42:23 +0900

    change canvas content

--- src/component/pages/Playground.js
+++ src/component/pages/Playground.js
@@ -13,6 +13,9 @@ export default class Playground extends Component {
       this.resizeCanvas();
     });
   }
+  mounted() {
+    this.resizeCanvas();
+  }
   resizeCanvas() {
     const canvas = document.getElementById('myCanvas');
     const parent = document.querySelector('main');
@@ -24,27 +27,16 @@ export default class Playground extends Component {
   draw() {
     const canvas = document.getElementById('myCanvas');
     const ctx = canvas.getContext('2d');
-    for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 3; j++) {
-        ctx.beginPath();
-        var x = 25 + j * 50; // x coordinate
-        var y = 25 + i * 50; // y coordinate
-        var radius = 20; // Arc radius
-        var startAngle = 0; // Starting point on circle
-        var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-        var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
-
-        ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-
-        if (i > 1) {
-          ctx.fill();
-        } else {
-          ctx.stroke();
-        }
+    for (var i = 0; i < 255; i++) {
+      for (var j = 0; j < 255; j++) {
+        ctx.fillStyle =
+          "rgb(" +
+          Math.floor(255 - 1 * i) +
+          ", " +
+          Math.floor(255 - 1 * j) +
+          ", 0)";
+        ctx.fillRect(j * 1, i * 1, 100, 25);
       }
     }
-  }
-  mounted() {
-    this.resizeCanvas();
   }
 }
